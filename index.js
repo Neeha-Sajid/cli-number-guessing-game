fcbd8fc485e827cc45d557d665b493c30566c6da
Neeha Sajid <farahsajid0@gmail.com>
2024-03-25 04:21:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//1.computer will generate a random number-DONE
+//2.user input for guessing number-DONE
+//3. compare user input with computer generated number and show result-DONE
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+console.log("WELCOME TO GUESSING GAME");
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 0-5:",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("CONGRATULATIONS, YOU GUESSED THE RIGHT NUMBER");
+}
+else {
+    console.log("YOU GUESSED WRONG NUMBER");
+}
